c41fd0226eb2d5b978c66367508a7f2878be3639
Daria <ddementieva@codenvy.com>
2016-01-14 17:58:29 +0200

    project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide"
+  });
+});
+// var myVideo = document.getElementById("video1"); 
+
+//function playPause() { 
+//    if (myVideo.paused) 
+//        myVideo.play(); 
+//    else 
+//        myVideo.pause(); 
+//} 
