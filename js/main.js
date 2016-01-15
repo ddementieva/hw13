7080eede33fe99d125761811d486f62704339e21
Daria <ddementieva@codenvy.com>
2016-01-15 18:33:49 +0200

    changed

--- js/main.js
+++ js/main.js
@@ -3,11 +3,25 @@ $(window).load(function() {
     animation: "slide"
   });
 });
-// var myVideo = document.getElementById("video1"); 
+
+
+
+
+
+// var myVideo = document.getElementById("video1");
 
 //function playPause() { 
 //    if (myVideo.paused) 
 //        myVideo.play(); 
 //    else 
 //        myVideo.pause(); 
-//} 
+//}
+
+
+
+//var tmpl = document.getElementById('header');
+//document.body.appendChild(tmpl.content.cloneNode(true));
+
+//$(document).ready(function(){
+//  $("#header").load("header.txt");
+//});
